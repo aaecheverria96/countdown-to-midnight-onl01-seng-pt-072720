e826cf5b0613ba763b4ce9a9536e912d017a7242
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-03 04:19:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,13 @@ def countdown (integer)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (integer)
+  sleep 5.seconds
+  integer = 10
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+  end
+    return "HAPPY NEW YEAR!"
+end 
